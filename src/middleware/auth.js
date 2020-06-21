5233905980434d1ef2302882443d4884478734ea
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-06-21 23:35:07 +0100

    added logout routes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,17 +4,21 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    console.log(token);
-
     const decoded = jwt.verify(token, 'test string');
+
+    console.log(token);
     console.log(decoded);
 
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
 
-    if(!user) {
-        throw new Error();
+    if (!user) {
+      throw new Error();
     }
 
+    req.token = token;
     req.user = user;
 
     next();
@@ -23,7 +27,6 @@ const auth = async (req, res, next) => {
       .status(401)
       .send({ error: 'you are are not authorized to access this route' });
   }
-
 };
 
 module.exports = auth;
--- src/routers/user.js
+++ src/routers/user.js
@@ -28,9 +28,37 @@ router.post('/users/login', async (req, res) => {
     );
     const token = await user.generateAuthToken();
 
+    await user.save();
+
     res.send({ user, token });
   } catch (e) {
     res.status(400).send();
+  }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+
+    await req.user.save();
+
+    res.send('token deleted');
+  } catch (e) {
+    req.status(500).send();
+  }
+});
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+
+    res.send('all tokens deleted');
+  } catch (e) {
+    req.status(500).send();
   }
 });
 
